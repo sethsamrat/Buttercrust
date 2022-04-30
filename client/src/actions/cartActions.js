78fd25fca79d9c629a50326952b221f066492f08
sethsamrat <31845987+sethsamrat@users.noreply.github.com>
2022-04-30 00:18:09 -0700

    Add files via upload

--- client/src/actions/cartActions.js
+++ client/src/actions/cartActions.js
@@ -1,22 +1,49 @@
-export const addToCart=(pizza, quantity, variant)=>(dispatch, getState)=>{
+export const addToCart=(pizza , quantity , varient)=>(dispatch , getState)=>{
+
 
 
     var cartItem = {
-
-        name : pizza.name,
+        name : pizza.name ,
         _id : pizza._id,
-        image : pizza.image,
-        variant : variant,
-        quantity : quantity,
-        prices : pizza.prices,
-        price : pizza.prices[0][variant] * quantity
-
+        image : pizza.image ,
+        varient : varient ,
+        quantity : Number(quantity) ,
+        prices : pizza.prices , 
+        price : pizza.prices[0][varient]* quantity
 
     }
-    
-    dispatch({type : 'ADD_TO_CART', payload : cartItem})
 
-    const cartItems =  getState().cartReducer.cartItems
-    localStorage.setItem('cartItems', JSON.stringify(cartItems))
+    if(cartItem.quantity>10)
+    {
+        alert('You cannot add more than 10 quantities')
+    }
+    else{
+        if(cartItem.quantity<1)
+        {
+            dispatch({type:'DELETE_FROM_CART' , payload:pizza}) 
+        }
+        else{
+            dispatch({type:'ADD_TO_CART' , payload : cartItem})
+        }
+       
+    }
+    
+
+    const cartItems = getState().cartReducer.cartItems
+    localStorage.setItem('cartItems' , JSON.stringify(cartItems))
+      
+
+
+}
+
+export const deleteFromCart=(pizza)=>(dispatch , getState)=>{
+
+
+     dispatch({type:'DELETE_FROM_CART' , payload:pizza})      
+     const cartItems = getState().cartReducer.cartItems
+     localStorage.setItem('cartItems' , JSON.stringify(cartItems))
+
+
+  
 
 }
